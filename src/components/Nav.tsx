41e9830cefea09c6ce38354e0b782c14180d7cb0
Jen-464 <jenlopez464@gmail.com>
2025-04-19 23:54:39 -0700

    navigation bar functionable and responsive

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,16 @@
 import { NavLink, Outlet } from "react-router-dom"
+import { useState } from "react"
 import logo from "../assets/shared/logo.svg"
 import line from "../assets/shared/line.svg"
 import "./Nav.css"
 
 const Nav = () => {
+    const [isNavVisible, setIsNavVisible] = useState(false);
+
+    const toggleNav = () => {
+        setIsNavVisible(prev => !prev);
+    };
+
     return (
         <>
             <header>
@@ -11,20 +18,15 @@ const Nav = () => {
                     <img src={logo} alt="space tourism star logo in white circle" />
                     <img src={line} alt="literally just a line for aesthetic purposes" />
                 </div>
-                <nav>
-                    <ul>
-                        <li>
-                            <NavLink to="/" className="barlow-condensed-8"> <span>00</span> Home</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/destination" className="barlow-condensed-8"> <span>01</span> Destination </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/crew" className="barlow-condensed-8">  <span>02</span> Crew </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/technology" className="barlow-condensed-8"> <span>03</span> Technology </NavLink>
-                        </li>
+                <button className="mobile-nav-toggle" aria-controls="primary-navigation" aria-expanded={isNavVisible} onClick={toggleNav}>
+                    <span className="sr-only">Menu</span>
+                </button>
+                <nav className={isNavVisible ? "nav-visible" : "nav-hidden"}>
+                    <ul id="primary-navigation">
+                        <NavLink to="/" className={({ isActive }) => `barlow-condensed-8 ${isActive ? "active" : ""}`}><li> <span aria-hidden="true"> 00 </span> HOME </li></NavLink>
+                        <NavLink to="/destination" className="barlow-condensed-8"><li> <span aria-hidden="true"> 01 </span> DESTINATION </li></NavLink>
+                        <NavLink to="/crew" className="barlow-condensed-8"><li> <span aria-hidden="true"> 02 </span> CREW </li></NavLink>
+                        <NavLink to="/technology" className="barlow-condensed-8"><li> <span aria-hidden="true"> 03 </span> TECHNOLOGY</li></NavLink>
                     </ul>
                 </nav>
             </header>
